2a8f8ce98ace76e8150729e01595557867f64202
andemiina <andemiia2020@gmail.com>
2024-09-10 14:20:24 +0300

    добавила переход таблицы к singleImageView

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Анна Демина on 10.09.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet private var imageView: UIImageView!
+}
