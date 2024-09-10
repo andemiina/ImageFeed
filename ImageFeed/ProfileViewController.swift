3d46310f0a814422f2a4f9dc4d371f270b2482a9
andemiina <andemiia2020@gmail.com>
2024-09-10 11:30:39 +0300

    сверастала profile scene

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Анна Демина on 09.09.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var profileImageView: UIImageView!
+    @IBOutlet private var exitButton: UIButton!
+    @IBOutlet weak var userStatusTextLabel: UILabel!
+    @IBOutlet weak var userLoginNameTextLabel: UILabel!
+    @IBOutlet weak var userFullNameTextLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+    @IBAction func exitButtonClicked(_ sender: Any) {
+    }
+    
+    
+}
+    
